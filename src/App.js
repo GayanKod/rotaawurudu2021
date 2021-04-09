e5f3e7d25a465a1e30ff0ce6136dda3083d8daaf
GayanKod <gayan.lakshan9@gmail.com>
2021-04-10 03:40:42 +0530

    Create Sign Up and Activate pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
+import{BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import SignUp from "./components/SignUp/Register";
+import Activate from "./components/SignUp/Activate";
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Navbar />
+      <Route path='/SignUp' exact render={props => <SignUp {...props} />} />
+      <Route path='/users/activate/:token' exact render={props => <Activate {...props} />} />
+    </Router>
     </div>
   );
 }
 
-export default App;
+export default App;  
