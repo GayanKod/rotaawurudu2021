21db81bbb041e442984bf03c428eefbf90272206
Nimna Perera <nimnaperera98@gmail.com>
2021-04-18 04:00:24 +0530

    Minor validation

--- src/components/LeaderBoard/LeaderBoard.jsx
+++ src/components/LeaderBoard/LeaderBoard.jsx
@@ -45,29 +45,35 @@ const LeaderBoard = ({ game }) => {
 							Error while loading data. Please try again.
 						</div>
 					) : (
-						<div className='max-w-7xl mx-auto py-6 md:px-40 sm:px-10 flex justify-center'>
-							<table className="table-fixed w-full">
-								<thead>
-									<tr className='bg-yellow-100'>
-										<th className="w-1/6 border border-yellow-500 px-4 py-2 text-yellow-600 font-medium"></th>
-										<th className="w-1/2 border border-yellow-500 px-4 py-2 text-yellow-600 font-medium">Player</th>
-										<th className="w-1/2 border border-yellow-500 px-4 py-2 text-yellow-600 font-medium">Score</th>
-									</tr>
-								</thead>
-								<tbody>
-									{
-										data.map((player, index) => (
-											<tr key={player._id} className={index % 2 == 0 ? 'bg-yellow-50' : 'bg-yellow-100'}>
-												<td className='py-2 md:px-8 sm:px-4 text-center border border-yellow-500 px-4 py-2 font-medium'>#{index + 1}</td>
-												<td className='py-2 px-8 text-center border border-yellow-500 px-4 py-2 font-medium'>{player.name} (<span className='text-gray-500 text-sm'>{player.house}</span>)</td>
-												<td className='py-2 px-8 text-center border border-yellow-500 px-4 py-2 font-medium'>{typeof player[game] === 'object' ? player[game].score.toFixed(3) : (game === 'GamaHarahaDiwima' ? player[game] : player[game].toFixed(3))}</td>
-											</tr>
-										))
-									}
+						data.length > 0 ? (
+							<div className='max-w-7xl mx-auto py-6 md:px-40 sm:px-10 flex justify-center'>
+								<table className="table-fixed w-full">
+									<thead>
+										<tr className='bg-yellow-100'>
+											<th className="w-1/6 border border-yellow-500 px-4 py-2 text-yellow-600 font-medium"></th>
+											<th className="w-1/2 border border-yellow-500 px-4 py-2 text-yellow-600 font-medium">Player</th>
+											<th className="w-1/2 border border-yellow-500 px-4 py-2 text-yellow-600 font-medium">Score</th>
+										</tr>
+									</thead>
+									<tbody>
+										{
+											data.map((player, index) => (
+												<tr key={player._id} className={index % 2 == 0 ? 'bg-yellow-50' : 'bg-yellow-100'}>
+													<td className='py-2 md:px-8 sm:px-4 text-center border border-yellow-500 px-4 py-2 font-medium'>#{index + 1}</td>
+													<td className='py-2 px-8 text-center border border-yellow-500 px-4 py-2 font-medium'>{player.name} (<span className='text-gray-500 text-sm'>{player.house}</span>)</td>
+													<td className='py-2 px-8 text-center border border-yellow-500 px-4 py-2 font-medium'>{typeof player[game] === 'object' ? player[game].score.toFixed(3) : (game === 'GamaHarahaDiwima' ? player[game] : player[game].toFixed(3))}</td>
+												</tr>
+											))
+										}
 
-								</tbody>
-							</table>
-						</div>
+									</tbody>
+								</table>
+							</div>
+						) : (
+							<div className='text-gray-500 text-center'>
+								No user played this game yet. Be the first to appear in this leaderboard.
+							</div>
+						)
 					)
 				)
 			}
